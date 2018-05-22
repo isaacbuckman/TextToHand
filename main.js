53f97fc833b1b3eb6780f7dbfb99461403d4539c
isaacbuckman <34870239+isaacbuckman@users.noreply.github.com>
2018-05-22 00:57:48 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,33 +1,20 @@
-var slider = document.getElementById("myRange");
-var output = document.getElementById("output");
-var sliderSeconds = document.getElementById("mySeconds");
-var outputSeconds = document.getElementById("outputseconds")
-var input = document.getElementById("input");
-// input.value = "In 1940, Stern began performing with Russian-born pianist Alexander Zakin, collaborating until 1977.[7] Within musical circles, Stern became renowned both for his recordings and for championing certain younger players. Among his discoveries were cellists Yo-Yo Ma and Jian Wang, and violinists Itzhak Perlman and Pinchas Zukerman. ";
-var button = document.getElementById("button");
-
-output.innerHTML = slider.value;
-outputSeconds.innerHTML = sliderSeconds.value;
-
-slider.oninput = function() {
-  output.innerHTML = this.value;
-}
-
-sliderSeconds.oninput = function() {
-  outputSeconds.innerHTML = this.value;
-}
+var wordsPerRead = document.getElementById("wordsPerRead");
+var secondDelay = document.getElementById("secondDelay");
+var textInput = document.getElementById("textInput");
+textInput.value = "In 1940, Stern began performing with Russian-born pianist Alexander Zakin, collaborating until 1977.[7] Within musical circles, Stern became renowned both for his recordings and for championing certain younger players. Among his discoveries were cellists Yo-Yo Ma and Jian Wang, and violinists Itzhak Perlman and Pinchas Zukerman. ";
+var playButton = document.getElementById("playButton");
 
 var interval;
 var speaking = false;
 
 function PlayPause() {
 	if (!speaking) {
-		var words = input.value.split(" ");
+		var words = textInput.value.split(" ");
 		var s = "";
 		var queue = []
 		for (var i = 0; i < words.length; i++) {
 			s += words[i] + " ";
-			if (i % (slider.value) == 0 && i !== 0) {
+			if ((i + 1) % wordsPerRead.options[wordsPerRead.selectedIndex].value == 0) {
 				var msg = new SpeechSynthesisUtterance(s);
 				queue.push(msg);
 				s = "";
@@ -36,38 +23,30 @@ function PlayPause() {
 				queue.push(msg);
 			}
 		}
-		button.innerHTML = "Cancel";
+		playButton.innerHTML = "Cancel";
 		speaking = true;
 		window.speechSynthesis.speak(queue.shift());
 		interval = setInterval(function(){
-			if (queue.length == 0) {
-				clearInterval(interval);
-				button.innerHTML = "Play"
-			} else {
-				window.speechSynthesis.speak(queue.shift());
-			}
-		},sliderSeconds.value * 1000);
+				var utterance = queue.shift();
+				if (queue.length == 0) {
+					utterance.onend = function(event) {
+						speaking = false;
+						window.speechSynthesis.cancel();
+						clearInterval(interval);
+						playButton.innerHTML = "Play";
+					}
+				}
+				window.speechSynthesis.speak(utterance);
+				
+		},secondDelay.options[secondDelay.selectedIndex].value * 1000);
 	} else {
 		speaking = false;
-		console.log(interval);
 		window.speechSynthesis.cancel();
 		clearInterval(interval);
-		button.innerHTML = "Play"
+		playButton.innerHTML = "Play";
 	}
 }
 
-/* if (!window.speechSynthesis.speaking) {
-	var msg = new SpeechSynthesisUtterance(input.value);
-	msg.onend = function(event) {
-		button.innerHTML = "Play";
-	}
-	window.speechSynthesis.speak(msg);
-	button.innerHTML = "Cancel";
-} else if (!window.speechSynthesis.paused) { 
-	window.speechSynthesis.pause();
-	button.innerHTML = "Continue";
- } else {
-	window.speechSynthesis.resume();
-	button.innerHTML = "Pause";
-} */
-
+function write() {
+	console.log("ji!")
+}
